175a2ec45655ae8715017d1c07c9bd4073ec3bce
bqcount <bqcount@gmail.com>
2024-04-17 19:44:15 +0100

    convert to React project,add basic structure

--- script.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('mousemove', (event) => {
-    const trail = document.createElement('div');
-    trail.className = 'starry';
-    trail.style.left = event.pageX + 'px';
-    trail.style.top = event.pageY + 'px';
-    document.getElementById('starry-sky').appendChild(trail);
-  });
-  
