b8d5609bc30f689fa9367731c74a542a8928cf1c
bqcount <bqcount@gmail.com>
2024-04-23 16:49:01 +0100

    stach

--- src/components/AboutMe/AboutMe.jsx
+++ src/components/AboutMe/AboutMe.jsx
@@ -1,8 +1,15 @@
-
+import './AboutMe.css'
 function AboutMe() {
   return (
-    <div>AboutMe</div>
+    <>
+    <div className="content neon-border">
+        <h2> Hello world... </h2>
+        <h3>I'm Daniela Romero</h3>
+        <h3>Full Stack developer</h3>
+        <h5>   and a lover of freedom, creativity and colors.</h5>
+    </div>
+    </>
   )
 }
 
-export default AboutMe
+export default AboutMe 
