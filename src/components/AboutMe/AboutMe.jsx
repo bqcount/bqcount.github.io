4e93d268b06ad09fa859940782df9fa125c29f15
bqcount <bqcount@gmail.com>
2024-05-08 21:29:09 +0100

    flex box

--- src/components/AboutMe/AboutMe.jsx
+++ src/components/AboutMe/AboutMe.jsx
@@ -4,15 +4,20 @@ function AboutMe() {
   return (
     <>
     <div className="content neon-border">
-     <div className='photo-profile'>
+    <div className='content-left'>
+
          <img src="src\assets\me.jpeg" alt="" style={{ width: '300px', height: '300px', borderRadius: '50%', objectFit: 'cover' }} />
+
       </div>
       <div className='content-right'>
+        <div>
 
         <h3> Hello world ... </h3>
         <h1> I'm Daniela Romero</h1>
-        <h3>Full Stack developer</h3>
-        
+        <div  className="glow-on-hover">
+          <h3>Full Stack developer</h3>
+        </div>
+
         <p>and a lover of freedom, creativity and colors.</p>
         <div className='icons-div'>
           <Link to="https://github.com/bqcount" className='network-links'>
@@ -24,6 +29,15 @@ function AboutMe() {
         </div>
       </div>
     </div>
+        </div>
+   
+
+    {/*   <div className="holder">  
+        <div className="bar1 left"></div>
+        <div className="bar top"></div>
+        <div className="bar1 right"></div>
+        <div className="bar bottom"></div>
+      </div> */}
     </>
   )
 }
