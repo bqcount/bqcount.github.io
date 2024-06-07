e1a33373e6143ac3f2961f53572a70b67a721310
bqcount <bqcount@gmail.com>
2024-06-07 09:12:36 +0100

    add bloody run

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -19,13 +19,18 @@ function Projects() {
           <div className='div-info'>
               <button className='button-info' onClick={()=>{setShowInfo(infoShow)}}>More info</button>
           </div>
-          {showInfo && (
-          <div className={`${infoShow}`}>
-            <p>Esta es la información adicional que deseas mostrar.</p>
-          </div>
-        )}
+          
         </div>
         
+        <div className="card">
+          <div className='div-img'>
+             <img src="src\assets\bloodyrun.png" alt="" className='img-project' />
+          </div>
+          <div className='div-info'>
+              <button className='button-info' onClick={()=>{setShowInfo(infoShow)}}>More info</button>
+          </div>
+          
+        </div>
     </div>
   )
 }
