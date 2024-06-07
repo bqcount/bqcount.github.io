0b3d94bbc4c07cf4510e27bee4479c96319664bd
bqcount <bqcount@gmail.com>
2024-06-07 09:14:32 +0100

    format

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -11,19 +11,14 @@ function Projects() {
           <div className='div-img'>
              <img src="src\assets\logocompicar.png" alt="" className='img-project' />
           </div>
-          <div className='div-info'>
-              <button className='button-info' onClick={()=>{setShowInfo(infoShow)}}>More info</button>
-          </div>
-          
+        
         </div>
         
         <div className="card">
           <div className='div-img'>
              <img src="src\assets\bloodyrun.png" alt="" className='img-project' />
           </div>
-          <div className='div-info'>
-              <button className='button-info' onClick={()=>{setShowInfo(infoShow)}}>More info</button>
-          </div>
+          
           
         </div>
     </div>
