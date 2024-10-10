5ec9311a9890d1b5de563146110b62165624f3b6
bqcount <bqcount@gmail.com>
2024-10-10 12:48:45 +0100

    1- Add contact icons 2- Fix mobile version 3- Add styles

--- src/components/AboutMe/AboutMe.jsx
+++ src/components/AboutMe/AboutMe.jsx
@@ -1,46 +1,42 @@
-import { Link } from 'react-router-dom'
-import './AboutMe.css'
+/* eslint-disable react/no-unescaped-entities */
 
+import "./AboutMe.css";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons';
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons'; // Correo electrónico
 
 function AboutMe() {
   return (
-    <div className='father-content'>
-    <div className="content ">
-    <div className='content-left'>
-
-         <img src="src\assets\me.jpeg" alt="" className='me-image'/>
-
+    <section className="content" aria-labelledby="about-me-heading">
+      <header className="sr-only" id="about-me-heading">About Me</header>
+      
+      <div className="content-left">
+        <img src="src/assets/me.jpeg" alt="Daniela Romero" className="me-image" />
+        <div className="icons-div">
+          <a className="link-header" href="https://github.com/bqcount" aria-label="GitHub">
+            <FontAwesomeIcon icon={faGithub} size="2x" />
+          </a>
+          <a className="link-header" href="https://www.linkedin.com/in/daniela-romero-garcia/" aria-label="LinkedIn">
+            <FontAwesomeIcon icon={faLinkedin} size="2x" />
+          </a>
+          <a className="link-header" href="mailto:daniela@example.com" aria-label="Email">
+            <FontAwesomeIcon icon={faEnvelope} size="2x" />
+          </a>
+        </div>
       </div>
-      <div className='content-right'>
+
+      <div className="content-right">
         <div>
-
-        <h3 className='h3-hello-world'> Hello world ... </h3>
-        <h1 className='h1-name'> I'm Daniela Romero</h1>
-        <div  className="glow-on-hover">
-          <h3 className='h3-fullstack'>Full Stack developer</h3>
-        </div>
-
-        <p>Passionate about novelty and creativity.</p>
-        <div className='div-contact'>
-        <button className='button-contact'>Contact me</button>
-        </div>
-        <div className='icons-div'>
- 
-
-          <Link to="https://github.com/bqcount" className='network-links'>
-          <img src="src\assets\icons8-github-50.png" alt="" className='git-linkedn' />
-          </Link>
-          <Link to="https://es.linkedin.com/in/daniela-romero-garcia" className='network-links'>
-          <img src="src\assets\icons8-linkedin-50.png" alt="" className='git-linkedn' />
-          </Link>
+          <h3 className="h3-hello-world">Hello world ...</h3>
+          <h1 className="h1-name">I'm Daniela Romero</h1>
+          <div className="glow-on-hover">
+            <h1 className="h3-fullstack">Full Stack Developer</h1>
+          </div>
+          <p>Passionate about novelty and creativity.</p>
         </div>
       </div>
-    </div>
-        </div>
-   
-
-    </div>
-  )
+    </section>
+  );
 }
 
-export default AboutMe 
+export default AboutMe;
